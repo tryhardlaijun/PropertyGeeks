3e225fd531a34a946e933e872f969238d678e8d4
Lee Yan Rong <66397701+YanRongLee@users.noreply.github.com>
2022-11-19 14:22:54 +0800

    Add files via upload

---
+++ sqlPMI/propertyTypeData.sql
@@ -0,0 +1,16 @@
+use project;
+
+INSERT INTO PropertyType (PType_ID, propertyType) VALUES
+	('1', 'Apartment'),
+	('2', 'Executive Condominium'),
+	('3', 'Condominium'),
+	('4', 'Strata Semi-detached'),
+	('5', 'Terrace'),
+	('6', 'Semi-detached'),
+	('7', 'Detached'),
+	('8', 'Strata Terrace'),
+	('9', 'Strata Detached'),
+	('10', 'Non-landed Properties'),
+	('11', 'Terrace House'),
+	('12', 'Detached House'),
+	('13', 'Semi-Detached House');
