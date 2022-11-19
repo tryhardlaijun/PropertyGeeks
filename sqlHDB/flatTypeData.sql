cb62fbac446689560ffebdfb43c5408476d19f20
Lee Yan Rong <66397701+YanRongLee@users.noreply.github.com>
2022-11-19 14:21:52 +0800

    Add files via upload

---
+++ sqlHDB/flatTypeData.sql
@@ -0,0 +1,30 @@
+-- use project DB
+use project;
+
+/*Insert FlatType Data*/
+INSERT INTO FlatType
+VALUES ('1', '1 ROOM');
+
+INSERT INTO FlatType
+VALUES ('2', '2 ROOM');
+
+INSERT INTO FlatType
+VALUES ('3', '3 ROOM');
+
+INSERT INTO FlatType
+VALUES ('4', '4 ROOM');
+
+INSERT INTO FlatType
+VALUES ('5', '5 ROOM');
+
+INSERT INTO FlatType
+VALUES ('6', 'EXECUTIVE');
+
+INSERT INTO FlatType
+VALUES ('7', 'MULTI-GENERATION');
+
+-- 7 lines
+select * from flattype
+
+
+
